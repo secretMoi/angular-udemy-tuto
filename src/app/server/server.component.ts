adaed316434814562d2a917787c0edb0c68915be
secretMoi <win.moi@hotmail.com>
2022-01-07 16:45:18 +0100

    Add basic component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+// indique à angular que c'est un component avec le décorateur @Component
+@Component({
+  selector: 'app-server', // balise pour utiliser ce component dans un fichier HTML
+  templateUrl: './server.component.html', // fichier HTML du component
+  styleUrls: ['./server.component.css'] // fichiers CSS du component
+})
+// export le component pour l'utiliser dans angular
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
