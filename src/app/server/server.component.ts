a721514efa2357c632fe66d36cce0c6007f57aac
secretMoi <win.moi@hotmail.com>
2022-01-10 15:50:57 +0100

    Ngstyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,12 +11,18 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   ngOnInit(): void {
   }
 
   getServerStatus() : string {
     return this.serverStatus;
+  }
+
+  getColor() : string {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
